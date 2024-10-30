4213ec6b883bda5e690c511d0ff737775a89472f
venkata-hari <xprogrammer.me@gmail.com>
2024-10-30 13:20:49 +0530

    aws commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import mainrouter from './Routes/Main.js'
 import cors from 'cors'
 const app = express()
 dotenv.config()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 app.use(cors())
 app.use(express.json())
 app.use('/', mainrouter)
